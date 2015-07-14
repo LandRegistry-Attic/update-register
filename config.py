2d286625bf33b865e9cd6d80b64c1b46ba935b8a
tsangsimon <simon.tsang@gmail.com>
2015-07-14 16:19:39 +0100

    sorting routes out to get title from currentRegisterDB

--- config.py
+++ config.py
@@ -5,15 +5,18 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     # format is dialect+driver://username:password@host:port/database
+    CURRENT_REGISTER_API = "http://localhost:5007"
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/workingregister'
     DEBUG = True
 
 class UnitTestConfig(Config):
     #Class needed so no messages not actually published by tests.
+    CURRENT_REGISTER_API = "http://localhost:5007"
     SQLALCHEMY_DATABASE_URI = ''
     DEBUG = True
 
 class TestConfig(Config):
+    CURRENT_REGISTER_API = "http://localhost:5007"
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', '')
     DEBUG = True
 
