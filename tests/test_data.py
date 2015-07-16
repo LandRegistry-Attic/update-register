8016be055a5d7beaead01c90c161175a76fb8cb1
tsangsimon <simon.tsang@gmail.com>
2015-07-16 13:48:16 +0100

    updating unit tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -232,6 +232,177 @@ TITLE_WITH_REPLACED_GROUP = {
     ]
 }
 
+TEST_REGISTER = {
+    "filed_plan_format": "PAPER",
+    "edition_date": "2003-07-25",
+    "last_app_timestamp": "2003-07-25T16:20:01+01:00",
+    "class": "Absolute",
+    "title_number": "AV239038",
+    "dlr": "Gloucester Office",
+    "application_reference": "F737RBF",
+    "groups": [
+        {
+            "category": "PROPERTY",
+            "entries": [
+                {
+                    "entry_id": "2015-02-03 12:48:29.760945",
+                    "sub_register": "A",
+                    "status": "Current",
+                    "sequence_number": 1,
+                    "role_code": "RDES",
+                    "language": "ENG",
+                    "category": "PROPERTY",
+                    "infills": [
+                        {
+                            "address": {
+                                "address_string": "the Incorporeal hereditaments known as The Manor or Lordship or Reputed Manor or Lordship of Littleton-upon-Severn in the former Parish of Littleton-upon-Severn",
+                                "auto_uppercase_override": "true"
+                            },
+                            "type": "Address"
+                        }
+                    ],
+                    "full_text": "The Freehold land being the Incorporeal hereditaments known as The Manor or Lordship or Reputed Manor or Lordship of Littleton-upon-Severn in the former Parish of Littleton-upon-Severn.",
+                    "entry_date": "1994-05-25",
+                    "template_text": "The Freehold land being *AD*"
+                }
+            ]
+        },
+        {
+            "category": "OWNERSHIP",
+            "entries": [
+                {
+                    "entry_id": "2015-02-03 12:48:31.495975",
+                    "sub_register": "B",
+                    "status": "Current",
+                    "sequence_number": 1,
+                    "role_code": "RPRO",
+                    "language": "ENG",
+                    "category": "OWNERSHIP",
+                    "infills": [
+                        {
+                            "proprietors": [
+                                {
+                                    "addresses": [
+                                        {
+                                            "postal_county": "London",
+                                            "address_string": "Flat 113, Eaton Rise, Eton College Road, London NW3 2DD",
+                                            "address_type": "UK",
+                                            "auto_uppercase_override": "true"
+                                        }
+                                    ],
+                                    "name": {
+                                        "surname": "HILL",
+                                        "forename": "MARIE",
+                                        "name_category": "PRIVATE INDIVIDUAL",
+                                        "auto_uppercase_override": "true"
+                                    }
+                                }
+                            ],
+                            "type": "Proprietor"
+                        }
+                    ],
+                    "full_text": "PROPRIETOR: %MARIE HILL% of Flat 113, Eaton Rise, Eton College Road, *London* NW3 2DD.",
+                    "entry_date": "2003-07-25",
+                    "template_text": "PROPRIETOR: *RP*"
+                }
+            ]
+        }
+    ],
+    "geometry": {
+
+    },
+    "tenure": "Freehold",
+    "raster_plan_quality": "INSUFFICIENT",
+    "districts": [
+        "SOUTH GLOUCESTERSHIRE"
+    ]
+}
+
+TEST_REGISTER2 = {
+    "filed_plan_format": "PAPER",
+    "edition_date": "2003-07-25",
+    "last_app_timestamp": "2003-07-25T16:20:01+01:00",
+    "class": "Absolute",
+    "title_number": "AV239040",
+    "dlr": "Gloucester Office",
+    "application_reference": "F737RBF",
+    "groups": [
+        {
+            "category": "PROPERTY",
+            "entries": [
+                {
+                    "entry_id": "2015-02-03 12:48:29.760945",
+                    "sub_register": "A",
+                    "status": "Current",
+                    "sequence_number": 1,
+                    "role_code": "RDES",
+                    "language": "ENG",
+                    "category": "PROPERTY",
+                    "infills": [
+                        {
+                            "address": {
+                                "address_string": "the Incorporeal hereditaments known as The Manor or Lordship or Reputed Manor or Lordship of Littleton-upon-Severn in the former Parish of Littleton-upon-Severn",
+                                "auto_uppercase_override": "true"
+                            },
+                            "type": "Address"
+                        }
+                    ],
+                    "full_text": "The Freehold land being the Incorporeal hereditaments known as The Manor or Lordship or Reputed Manor or Lordship of Littleton-upon-Severn in the former Parish of Littleton-upon-Severn.",
+                    "entry_date": "1994-05-25",
+                    "template_text": "The Freehold land being *AD*"
+                }
+            ]
+        },
+        {
+            "category": "OWNERSHIP",
+            "entries": [
+                {
+                    "entry_id": "2015-02-03 12:48:31.495975",
+                    "sub_register": "B",
+                    "status": "Current",
+                    "sequence_number": 1,
+                    "role_code": "RPRO",
+                    "language": "ENG",
+                    "category": "OWNERSHIP",
+                    "infills": [
+                        {
+                            "proprietors": [
+                                {
+                                    "addresses": [
+                                        {
+                                            "postal_county": "London",
+                                            "address_string": "Flat 113, Eaton Rise, Eton College Road, London NW3 2DD",
+                                            "address_type": "UK",
+                                            "auto_uppercase_override": "true"
+                                        }
+                                    ],
+                                    "name": {
+                                        "surname": "HILL",
+                                        "forename": "MARIE",
+                                        "name_category": "PRIVATE INDIVIDUAL",
+                                        "auto_uppercase_override": "true"
+                                    }
+                                }
+                            ],
+                            "type": "Proprietor"
+                        }
+                    ],
+                    "full_text": "PROPRIETOR: %MARIE HILL% of Flat 113, Eaton Rise, Eton College Road, *London* NW3 2DD.",
+                    "entry_date": "2003-07-25",
+                    "template_text": "PROPRIETOR: *RP*"
+                }
+            ]
+        }
+    ],
+    "geometry": {
+
+    },
+    "tenure": "Freehold",
+    "raster_plan_quality": "INSUFFICIENT",
+    "districts": [
+        "SOUTH GLOUCESTERSHIRE"
+    ]
+}
 # dump these json arguments to string to fire at http
 ENTRY_AMENDMENT = json.dumps({
     "entry_id": "998",
