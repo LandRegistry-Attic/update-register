38d5f2df6382b6a173cbf40947cf2edcc7ea6530
Matt Shaw <matthew.shaw@landregistry.gsi.gov.uk>
2015-08-11 13:30:21 +0100

    update env vars

--- config.py
+++ config.py
@@ -1,24 +1,21 @@
 import os
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/workingregister'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres@localhost/workingregister')
     DEBUG = False
 
 class DevelopmentConfig(Config):
     # format is dialect+driver://username:password@host:port/database
     CURRENT_REGISTER_API = "http://localhost:5007"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/workingregister'
     DEBUG = True
 
 class UnitTestConfig(Config):
     #Class needed so no messages not actually published by tests.
     CURRENT_REGISTER_API = "http://localhost:5007"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/workingregister'
     DEBUG = True
 
 class TestConfig(Config):
     CURRENT_REGISTER_API = "http://localhost:5007"
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', '')
     DEBUG = True
 
 class PreproductionConfig(Config):
