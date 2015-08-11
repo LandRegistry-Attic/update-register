83dd863f3e0bde6994dc86e05d49c618f0f4b67b
Matt Shaw <matthew.shaw@landregistry.gsi.gov.uk>
2015-08-11 13:48:06 +0100

    update current register api env var

--- config.py
+++ config.py
@@ -2,20 +2,17 @@ import os
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres@localhost/workingregister')
+    CURRENT_REGISTER_API = os.getenv('CURRENT_REGISTER_API', 'http://localhost:5007')
     DEBUG = False
 
 class DevelopmentConfig(Config):
-    # format is dialect+driver://username:password@host:port/database
-    CURRENT_REGISTER_API = "http://localhost:5007"
     DEBUG = True
 
 class UnitTestConfig(Config):
     #Class needed so no messages not actually published by tests.
-    CURRENT_REGISTER_API = "http://localhost:5007"
     DEBUG = True
 
 class TestConfig(Config):
-    CURRENT_REGISTER_API = "http://localhost:5007"
     DEBUG = True
 
 class PreproductionConfig(Config):
